bf7105b5c4152bb915f084eedbc5fa3e149a1c34
wisehero <godjw96@gmail.com>
2025-03-26 12:21:36 +0900

    feat : ApiControllerAdvice에 ExceptionHandler 추가 및 ErrorResponse 수정

--- src/main/java/io/hhplus/tdd/ApiControllerAdvice.java
+++ src/main/java/io/hhplus/tdd/ApiControllerAdvice.java
@@ -1,14 +1,44 @@
 package io.hhplus.tdd;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 
 @RestControllerAdvice
 class ApiControllerAdvice extends ResponseEntityExceptionHandler {
-    @ExceptionHandler(value = Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(Exception e) {
-        return ResponseEntity.status(500).body(new ErrorResponse("500", "에러가 발생했습니다."));
-    }
+
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(
+		MethodArgumentTypeMismatchException e) {
+		return ResponseEntity.status(400).body(new ErrorResponse("400", "올바르지 않은 타입입니다."));
+	}
+
+	@ExceptionHandler(value = ConstraintViolationException.class)
+	public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException e) {
+		Map<String, String> errors = e.getConstraintViolations().stream()
+			.collect(Collectors.toMap(
+				cv -> cv.getPropertyPath().toString(),
+				ConstraintViolation::getMessage
+			));
+		return ResponseEntity.status(400).body(new ErrorResponse("400", "유효성 검사 실패", errors));
+	}
+
+	@ExceptionHandler(value = IllegalArgumentException.class)
+	public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+		return ResponseEntity.status(400).body(new ErrorResponse("400", e.getMessage()));
+	}
+
+	@ExceptionHandler(value = Exception.class)
+	public ResponseEntity<ErrorResponse> handleException(Exception e) {
+		return ResponseEntity.status(500).body(new ErrorResponse("500", "에러가 발생했습니다."));
+	}
 }
--- src/main/java/io/hhplus/tdd/ErrorResponse.java
+++ src/main/java/io/hhplus/tdd/ErrorResponse.java
@@ -1,7 +1,16 @@
 package io.hhplus.tdd;
 
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ErrorResponse(
-        String code,
-        String message
+	String code,
+	String message,
+	Map<String, String> validation
 ) {
+	public ErrorResponse(String code, String message) {
+		this(code, message, null);
+	}
 }
