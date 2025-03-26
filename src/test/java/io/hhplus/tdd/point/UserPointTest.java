554fb37f23d98c56a87c9e9c02310a4f23fe9e32
wisehero <godjw96@gmail.com>
2025-03-26 20:46:22 +0900

    refactor : UserPoint 도메인 단위 테스트 리팩토링

--- src/test/java/io/hhplus/tdd/point/UserPointTest.java
+++ src/test/java/io/hhplus/tdd/point/UserPointTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 class UserPointTest {
 
 	@Test
-	@DisplayName("사용자 포인트를 충전한다.")
+	@DisplayName("포인트가 0인 사용자가 100_000 포인트를 충전하면 100_000 포인트가 정상적으로 충전된다.")
 	void chargeUserPoint() {
 		// given
 		UserPoint userPoint = UserPoint.empty(1L);
@@ -24,7 +24,7 @@ class UserPointTest {
 	}
 
 	@Test
-	@DisplayName("만약 충전 포인트가 최대 한도를 초과하면, 예외가 발생한다.")
+	@DisplayName("만약 충전 포인트가 최대 한도를 초과하면, 포인트가 충전되지 않고 IllegalArgumentException 예외가 발생한다.")
 	void chargePointOverMaxLimit() {
 		// given
 		UserPoint userPoint = UserPoint.empty(1L);
@@ -35,11 +35,12 @@ class UserPointTest {
 		assertThatThrownBy(() -> userPoint.charge(chargePoint))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("최대 포인트 한도는 %d 입니다. 입력값: %d".formatted(MAX_CHARGE_POINT, chargePoint));
+		assertThat(userPoint.point()).isEqualTo(0);
 	}
 
 	@ParameterizedTest
 	@ValueSource(longs = {0, -1})
-	@DisplayName("만약 충전 포인트가 0이거나 음수라면, 예외가 발생한다.")
+	@DisplayName("만약 충전 포인트가 0이거나 음수라면, 포인트가 충전되지 않고 IllegalArgumentException 예외가 발생한다.")
 	void ifChargePointUnderZero(long chargePoint) {
 		// given
 		UserPoint userPoint = UserPoint.empty(1L);
@@ -48,53 +49,59 @@ class UserPointTest {
 		assertThatThrownBy(() -> userPoint.charge(chargePoint))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("충전할 포인트는 0 보다 커야 합니다. 입력값: %d".formatted(chargePoint));
+		assertThat(userPoint.point()).isEqualTo(0);
 	}
 
 	@ParameterizedTest
-	@ValueSource(longs = {10_000, 20_000})
-	@DisplayName("사용자 포인트는 최대 사용 가능 포인트 내에서 사용할 수 있다.")
+	@ValueSource(longs = {5_000, 10_000})
+	@DisplayName("포인트를 사용자가 가진 포인트 범위 내에서 사용되면 그만큼 포인트가 차감된다.")
 	void useUserPoint(long usePoint) {
 		// given
-		UserPoint userPoint = new UserPoint(1L, 20_000, System.currentTimeMillis());
+		UserPoint userPoint = new UserPoint(1L, 10_000, System.currentTimeMillis());
 
 		// when
 		UserPoint usedPoint = userPoint.use(usePoint);
 
 		// then
-		assertThat(usedPoint.point()).isEqualTo(10_000);
+		assertThat(usedPoint.point()).isEqualTo(userPoint.point() - usePoint);
 	}
 
 	@ParameterizedTest
 	@ValueSource(longs = {0, -1})
-	@DisplayName("만약 사용하고자 하는 포인트 0이거나 음수라면, 예외가 발생한다.")
+	@DisplayName("만약 사용하고자 하는 포인트 0이거나 음수라면, 포인트는 변함없고 IllegalArgumentException 예외가 발생한다.")
 	void ifUsePointUnderZero(long usePoint) {
 		// given
-		UserPoint userPoint = new UserPoint(1L, 20_000, System.currentTimeMillis());
+		long initialPoint = 10_000L;
+		UserPoint userPoint = new UserPoint(1L, initialPoint, System.currentTimeMillis());
 
 		// when then
 		assertThatThrownBy(() -> userPoint.use(usePoint))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("사용할 포인트는 0 보다 커야 합니다. 입력값: %d".formatted(usePoint));
+		assertThat(userPoint.point()).isEqualTo(initialPoint);
 	}
 
 	@Test
-	@DisplayName("만약 사용하고자 하는 포인트가 현재 보유한 포인트를 초과하면, 예외가 발생한다.")
+	@DisplayName("만약 사용하고자 하는 포인트가 현재 보유한 포인트를 초과하면, 포인트는 변함없고 IllegalArgumentException 예외가 발생한다.")
 	void ifUsePointOverCurrentPoint() {
 		// given
-		UserPoint userPoint = new UserPoint(1L, 5_000, System.currentTimeMillis());
-		long usePoint = 5_001;
+		long initialPoint = 5_000L;
+		UserPoint userPoint = new UserPoint(1L, initialPoint, System.currentTimeMillis());
+		long usePoint = 5_001L;
 
 		// when then
 		assertThatThrownBy(() -> userPoint.use(usePoint))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("갖고 있는 포인트를 초과해서 사용할 수 없습니다. 입력값: %d, 현재 포인트 잔고: %d".formatted(usePoint, userPoint.point()));
+		assertThat(userPoint.point()).isEqualTo(initialPoint);
 	}
 
 	@Test
-	@DisplayName("만약 사용하고자 하는 포인트가 최대 사용가능한 포인트를 초과하면, 예외가 발생한다.")
+	@DisplayName("만약 사용하고자 하는 포인트가 최대 사용가능한 포인트를 초과하면, 포인트는 변함없고 IllegalArgumentException 예외가 발생한다.")
 	void ifUsePointOverMaxLimit() {
 		// given
-		UserPoint userPoint = new UserPoint(1L, 20_000, System.currentTimeMillis());
+		long initialPoint = 20_000L;
+		UserPoint userPoint = new UserPoint(1L, initialPoint, System.currentTimeMillis());
 		long MAX_USE_POINT = 10_000;
 		long usePoint = 10_001;
 
@@ -102,5 +109,6 @@ class UserPointTest {
 		assertThatThrownBy(() -> userPoint.use(usePoint))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("최대 사용 가능한 포인트는 %d입니다. 입력값: %d".formatted(MAX_USE_POINT, usePoint));
+		assertThat(userPoint.point()).isEqualTo(initialPoint);
 	}
 }
