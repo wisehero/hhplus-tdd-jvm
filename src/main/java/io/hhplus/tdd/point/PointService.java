ae38db967133e07acb641c0e866b47bfe63dc633
wisehero <godjw96@gmail.com>
2025-03-27 17:51:29 +0900

    feat : ConcurrentHashMap, ReentrantLock 으로 동시성 테스트

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,6 +1,9 @@
 package io.hhplus.tdd.point;
 
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.springframework.stereotype.Service;
 
@@ -13,29 +16,49 @@ public class PointService {
 	private final PointRepository pointRepository;
 	private final PointHistoryRepository pointHistoryRepository;
 
+	private final ConcurrentHashMap<Long, Lock> userIdLockMap = new ConcurrentHashMap<>();
+
 	public UserPoint findPointById(long id) {
 		return pointRepository.findById(id);
 	}
 
-	public synchronized UserPoint chargePoint(long id, long amount) {
-		UserPoint findUserPoint = pointRepository.findById(id);
+	public UserPoint chargePoint(long id, long amount) {
 
-		UserPoint charged = findUserPoint.charge(amount);
-		pointHistoryRepository.saveUserPoint(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
+		Lock userIdLock = getUserIdKey(id);
 
-		return pointRepository.saveOrUpdate(charged.id(), charged.point());
+		userIdLock.lock();
+		try {
+			UserPoint findUserPoint = pointRepository.findById(id);
+
+			UserPoint charged = findUserPoint.charge(amount);
+			pointHistoryRepository.saveUserPoint(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
+			return pointRepository.saveOrUpdate(charged.id(), charged.point());
+		} finally {
+			userIdLock.unlock();
+		}
 	}
 
-	public synchronized UserPoint usePoint(long id, long amount) {
-		UserPoint findUserPoint = pointRepository.findById(id);
+	public UserPoint usePoint(long id, long amount) {
+		Lock userIdLock = getUserIdKey(id);
 
-		UserPoint used = findUserPoint.use(amount);
-		pointHistoryRepository.saveUserPoint(id, amount, TransactionType.USE, System.currentTimeMillis());
+		userIdLock.lock();
+		try {
+			UserPoint findUserPoint = pointRepository.findById(id);
 
-		return pointRepository.saveOrUpdate(used.id(), used.point());
+			UserPoint used = findUserPoint.use(amount);
+			pointHistoryRepository.saveUserPoint(id, amount, TransactionType.USE, System.currentTimeMillis());
+
+			return pointRepository.saveOrUpdate(used.id(), used.point());
+		} finally {
+			userIdLock.unlock();
+		}
 	}
 
 	public List<PointHistory> findPointHistoriesOfUser(long userId) {
 		return pointHistoryRepository.findAllHistoryByUserId(userId);
+	}
+
+	private Lock getUserIdKey(long userId) {
+		return userIdLockMap.computeIfAbsent(userId, id -> new ReentrantLock(true));
 	}
 }
--- src/test/java/io/hhplus/tdd/point/PointServiceConcurrencyTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceConcurrencyTest.java
@@ -3,10 +3,13 @@ package io.hhplus.tdd.point;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,12 +33,12 @@ public class PointServiceConcurrencyTest {
 	private final long FIXED_AMOUNT = 100L;
 
 	@Test
-	@DisplayName("PointService#chargePoint를 실행해서 한 명의 유저 포인트 충전을 동시에 50개 요청하면 포인트는 5_000이 되고, 이력도 50개가 남지 않는다.")
-	public void pointServiceConcurrencyTest() throws InterruptedException {
+	@DisplayName("0 포인트를 가진 사용자를 대상으로 100 포인트를 충전하는 요청이 20개 동시에 들어오면 포인트는 2_000이 되고 이력도 20개가 남는다.")
+	public void chargePointConcurrencyTest() throws InterruptedException {
 
 		// given
 		long userId = 1L;
-		int threadCount = 50;
+		int threadCount = 20;
 		pointRepository.saveOrUpdate(userId, 0L);
 
 		ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
@@ -59,18 +62,107 @@ public class PointServiceConcurrencyTest {
 		List<PointHistory> allHistoryByUserId = pointHistoryRepository.findAllHistoryByUserId(userId);
 
 		assertAll(
-			() -> assertThat(findUserPoint.point()).isEqualTo(FIXED_AMOUNT * 50),
+			() -> assertThat(findUserPoint.point()).isEqualTo(FIXED_AMOUNT * threadCount),
 			() -> assertThat(allHistoryByUserId.size()).isEqualTo(threadCount)
 		);
 	}
 
 	@Test
-	@DisplayName("PointService#usePoint를 실행해서 한 명의 유저 포인트 충전을 동시에 50개 요청하면 충전 포인트는 5_000이고 이력은 50개가 생긴다.")
-	void pointServiceConcurrencyTestWithSync() throws InterruptedException {
+	@DisplayName("2명의 유저를 대상으로 100 포인트를 충전하는 요청을 각 20번 동시 요청하면 두 유저의 포인트는 각 2_000이고 이력도 20개씩 남는다.")
+	public void chargePointConcurrencyTestMultiUser() throws InterruptedException {
+
+		// given
+		long userId1 = 1L;
+		long userId2 = 2L;
+		int threadCount = 20;
+		pointRepository.saveOrUpdate(userId1, 0L);
+		pointRepository.saveOrUpdate(userId2, 0L);
+
+		ExecutorService executorService = Executors.newFixedThreadPool(threadCount * 2);
+		CountDownLatch latch = new CountDownLatch(threadCount * 2);
+
+		// when
+		for (int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					pointService.chargePoint(userId1, FIXED_AMOUNT);
+				} finally {
+					latch.countDown();
+				}
+			});
+
+			executorService.submit(() -> {
+				try {
+					pointService.chargePoint(userId2, FIXED_AMOUNT);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+		latch.await();
+		executorService.shutdown();
+
+		// then
+		UserPoint findUser1Point = pointRepository.findById(userId1);
+		UserPoint findUser2Point = pointRepository.findById(userId2);
+		List<PointHistory> allHistoryByUserId1 = pointHistoryRepository.findAllHistoryByUserId(userId1);
+		List<PointHistory> allHistoryByUserId2 = pointHistoryRepository.findAllHistoryByUserId(userId2);
+
+		assertAll(
+			() -> assertThat(findUser1Point.point()).isEqualTo(FIXED_AMOUNT * threadCount),
+			() -> assertThat(findUser2Point.point()).isEqualTo(FIXED_AMOUNT * threadCount),
+			() -> assertThat(allHistoryByUserId1.size()).isEqualTo(threadCount),
+			() -> assertThat(allHistoryByUserId2.size()).isEqualTo(threadCount)
+		);
+	}
+
+	@Test
+	@DisplayName("90,000 포인트를 가진 사용자를 대상으로 1,000 포인트를 충전하는 요청이 15개 동시에 들어오면 10개는 성공하고 5개는 IllegalStateException 예외가 발생한다.")
+	public void chargePointConcurrencyExceptionTest() throws InterruptedException {
+		// given
+		long userId = 1L;
+		long initialAmount = 90_000L;
+		long chargeAmount = 1_000L;
+		pointRepository.saveOrUpdate(userId, initialAmount);
+		int threadCount = 15; // 15 * 1,000 = 15,000 + 90,000 = 105,000 5,000 만큼을 초과하고 5번 실패해야한다.
+
+		ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+		CountDownLatch latch = new CountDownLatch(threadCount);
+		AtomicInteger successCount = new AtomicInteger(0);
+		List<Exception> exceptions = Collections.synchronizedList(new ArrayList<>());
+
+		// when
+		for (int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					pointService.chargePoint(userId, chargeAmount);
+					successCount.incrementAndGet();
+				} catch (Exception e) {
+					exceptions.add(e);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+		latch.await();
+		executorService.shutdown();
+
+		// then
+		UserPoint findUserPoint = pointRepository.findById(userId);
+		assertAll(
+			() -> assertThat(findUserPoint.point()).isEqualTo(100_000L),
+			() -> assertThat(successCount.get()).isEqualTo(10),
+			() -> assertThat(exceptions.size()).isEqualTo(5)
+		);
+	}
+
+	@Test
+	@DisplayName("10_000 포인트를 가진 사용자를 대상으로 100 포인트를 사용하는 요청이 20개 동시에 들어오면 포인트는 8_000이 되고 이력도 20개가 남는다.")
+	void usePointConcurrentcyTest() throws InterruptedException {
 		// given
 		long userId = 1L;
 		long initialAmount = 10_000L;
-		int threadCount = 50;
+		int threadCount = 20;
 		pointRepository.saveOrUpdate(userId, initialAmount);
 		ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
 		CountDownLatch latch = new CountDownLatch(threadCount);
@@ -93,8 +185,98 @@ public class PointServiceConcurrencyTest {
 		UserPoint findUserPoint = pointRepository.findById(userId);
 		List<PointHistory> allHistoryByUserId = pointHistoryRepository.findAllHistoryByUserId(userId);
 		assertAll(
-			() -> assertThat(findUserPoint.point()).isEqualTo(initialAmount - (FIXED_AMOUNT * 50)),
+			() -> assertThat(findUserPoint.point()).isEqualTo(initialAmount - (FIXED_AMOUNT * threadCount)),
 			() -> assertThat(allHistoryByUserId.size()).isEqualTo(threadCount)
+		);
+	}
+
+	@Test
+	@DisplayName("2명의 유저를 대상으로 100 포인트 사용하는 요청을 각 20번 동시 요청하면 두 유저의 포인트는 각 2_000씩 차감되고 이력도 20개씩 남는다.")
+	public void usePointConcurrencyTestMultiUser() throws InterruptedException {
+
+		// given
+		long userId1 = 1L;
+		long userId2 = 2L;
+		int threadCount = 20;
+		long initialAmount = 10_000L;
+		pointRepository.saveOrUpdate(userId1, initialAmount);
+		pointRepository.saveOrUpdate(userId2, initialAmount);
+
+		ExecutorService executorService = Executors.newFixedThreadPool(threadCount * 2);
+		CountDownLatch latch = new CountDownLatch(threadCount * 2);
+
+		// when
+		for (int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					pointService.usePoint(userId1, FIXED_AMOUNT);
+				} finally {
+					latch.countDown();
+				}
+			});
+
+			executorService.submit(() -> {
+				try {
+					pointService.usePoint(userId2, FIXED_AMOUNT);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+		latch.await();
+		executorService.shutdown();
+
+		// then
+		UserPoint findUser1Point = pointRepository.findById(userId1);
+		UserPoint findUser2Point = pointRepository.findById(userId2);
+		List<PointHistory> allHistoryByUserId1 = pointHistoryRepository.findAllHistoryByUserId(userId1);
+		List<PointHistory> allHistoryByUserId2 = pointHistoryRepository.findAllHistoryByUserId(userId2);
+
+		assertAll(
+			() -> assertThat(findUser1Point.point()).isEqualTo(initialAmount - (FIXED_AMOUNT * threadCount)),
+			() -> assertThat(findUser2Point.point()).isEqualTo(initialAmount - (FIXED_AMOUNT * threadCount)),
+			() -> assertThat(allHistoryByUserId1.size()).isEqualTo(threadCount),
+			() -> assertThat(allHistoryByUserId2.size()).isEqualTo(threadCount)
+		);
+	}
+
+	@Test
+	@DisplayName("10,000 포인트를 가진 사용자를 대상으로 2,000 포인트를 사용하는 요청이 동시에 10번이 들어오면 5번은 성공해서 포인트가 0원이 되고 5번은 IlleagalArgumentException 예외가 발생한다.")
+	public void usePointConcurrencyExceptionTest() throws InterruptedException {
+		// given
+		long userId = 1L;
+		long initialAmount = 10_000L;
+		long useAmount = 2_000L;
+		pointRepository.saveOrUpdate(userId, initialAmount);
+		int threadCount = 10;
+
+		ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+		CountDownLatch latch = new CountDownLatch(threadCount);
+		AtomicInteger successCount = new AtomicInteger(0);
+		List<Exception> exceptions = Collections.synchronizedList(new ArrayList<>());
+
+		// when
+		for (int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					pointService.usePoint(userId, useAmount);
+					successCount.incrementAndGet();
+				} catch (Exception e) {
+					exceptions.add(e);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+		latch.await();
+		executorService.shutdown();
+
+		// then
+		UserPoint findUserPoint = pointRepository.findById(userId);
+		assertAll(
+			() -> assertThat(findUserPoint.point()).isEqualTo(0L),
+			() -> assertThat(successCount.get()).isEqualTo(5),
+			() -> assertThat(exceptions.size()).isEqualTo(5)
 		);
 	}
 }
